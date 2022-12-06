cab509d4dfdb1a7da8eee90710f40e36f07dda7f
Prashik Moon <moonprashik17@gmail.com>
2022-12-06 17:42:10 +0530

    Personal Portfolio

---
+++ portfolio/src/App.js
@@ -0,0 +1,24 @@
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Portfolio from "./components/Portfolio";
+import SocialLinks from "./components/SocialLinks";
+import Contact from "./components/Contact";
+
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Home/>
+      <About/>
+      <Portfolio/>
+      <Skills/>
+      <SocialLinks/>
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
